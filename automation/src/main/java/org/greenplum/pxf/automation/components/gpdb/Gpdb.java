aaf6bd76a2d61f807a4e5ffeaf6539e0b7378399
Nikolay Antonov <ostinru@gmail.com>
2025-11-26 00:34:21 +0500

    Fix: automation tests should be able to detect Cloudberry version

--- automation/src/main/java/org/greenplum/pxf/automation/components/gpdb/Gpdb.java
+++ automation/src/main/java/org/greenplum/pxf/automation/components/gpdb/Gpdb.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class Gpdb extends DbSystemObject {
 
 	private static final String DEFAULT_PORT = "5432";
-	private static final String GREENPLUM_DATABASE_PREFIX = "Greenplum Database ";
+    private static final String GREENPLUM_DATABASE_PREFIX = "Apache Cloudberry "; // for 2.x
 	private static final String IF_NOT_EXISTS_OPTION = "IF NOT EXISTS";
 
 	private String sshUserName;
