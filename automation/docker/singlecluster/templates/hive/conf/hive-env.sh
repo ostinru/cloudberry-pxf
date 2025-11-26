0621e292195eb404d5b513d9e208fdfbb39e7e7e
Nikolay Antonov <ostinru@gmail.com>
2025-11-27 01:28:50 +0500

    Automation: allow Hive to consume more RAM

--- automation/docker/singlecluster/templates/hive/conf/hive-env.sh
+++ automation/docker/singlecluster/templates/hive/conf/hive-env.sh
@@ -1,6 +1,7 @@
 # load singlecluster environment
 . $GPHD_ROOT/bin/gphd-env.sh
 
+export HADOOP_OPTS="$HADOOP_OPTS -Xmx1024m -Xms128m $COMMON_JAVA_OPTS"
 export HIVE_OPTS="-hiveconf derby.stream.error.file=$LOGS_ROOT/derby.log -hiveconf javax.jdo.option.ConnectionURL=jdbc:derby:;databaseName=$HIVE_STORAGE_ROOT/metastore_db;create=true"
 export HIVE_SERVER_OPTS="-hiveconf derby.stream.error.file=$LOGS_ROOT/derby.log -hiveconf ;databaseName=$HIVE_STORAGE_ROOT/metastore_db;create=true"
 export HADOOP_HOME=$HADOOP_ROOT
