fe63f6bb4198394a918146cd311e0fa9bb7e7afe
Nikolay Antonov <ostinru@yandex-team.ru>
2026-07-14 02:41:15 +0500

    cleanup

--- ci/docker/pxf-cbdb-dev/common/script/run_tests.sh
+++ ci/docker/pxf-cbdb-dev/common/script/run_tests.sh
@@ -627,9 +627,6 @@ run_single_group() {
     features_fdw)
       feature_test "true"
       ;;
-    pxf_extension)
-      pxf_extension_test
-      ;;
     load)
       bench_prepare_env
       load_test
