75c066f119eed93d681fdcbf6e77acf995572c43
Nikolay Antonov <ostinru@yandex-team.ru>
2026-09-15 19:11:16 +0500

    Enable HiveTest scenarios in FDW automation

--- automation/src/test/java/org/apache/cloudberry/pxf/automation/features/hive/HiveTest.java
+++ automation/src/test/java/org/apache/cloudberry/pxf/automation/features/hive/HiveTest.java
@@ -1,5 +1,6 @@
 package org.apache.cloudberry.pxf.automation.features.hive;
 
+import annotations.WorksWithFDW;
 import jsystem.framework.system.SystemManagerImpl;
 import org.apache.cloudberry.pxf.automation.components.hive.Hive;
 import org.apache.cloudberry.pxf.automation.enums.EnumPxfDefaultProfiles;
@@ -20,6 +21,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@WorksWithFDW
 public class HiveTest extends HiveBaseTest {
 
     private static final String HIVE_PARTITIONED_SKEWED_TABLE = "hive_partitioned_skewed_table";
